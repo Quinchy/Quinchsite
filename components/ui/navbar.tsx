fbda35d0a4470e7fb6eb18270c13c2e577c14317
Quinchy <deguzmancyriljames@gmail.com>
2024-08-08 20:55:56 +0800

    first commit

---
+++ components/ui/navbar.tsx
@@ -0,0 +1,53 @@
+// components/ui/navbar.tsx
+
+import { useEffect, useState } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <motion.div
+    initial={{ backgroundColor: 'var(--color-background)', width: '40rem', boxShadow: 'none' }}
+    animate={{
+        backgroundColor: scrolled ? 'var(--color-background-opacity2)' : 'var(--color-background)',
+        width: scrolled ? '35rem' : '40rem',
+        boxShadow: scrolled ? '0px 4px 15px rgba(0, 0, 0, 0.7)' : 'none',
+        transition: {
+            duration: 0.3,
+            backgroundColor: { duration: 0.3 },
+            width: { duration: 0.3 },
+            boxShadow: { duration: 0.3 },
+            ease: 'easeInOut',
+        },
+    }}
+    className="fixed mt-2 top-0 left-1/2 transform -translate-x-1/2 z-50 py-6 rounded-xl"
+>
+    <div className="container mx-auto">
+        <div className="flex justify-center items-center gap-20 text-lg tracking-tighter font-semibold text-[--color-main-light]">
+            <a href="" className="px-4">Home</a>
+            <a href="" className="px-4">About</a>
+            <a href="" className="px-4">Projects</a>
+        </div>
+    </div>
+</motion.div>
+
+  );
+};
+
+export default Navbar;
