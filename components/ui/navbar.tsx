227564a93e9f320b1aa0441b4a36d5d74395f7f7
Quinchy <deguzmancyriljames@gmail.com>
2024-08-08 21:25:17 +0800

    .

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             ease: 'easeInOut',
         },
     }}
-    className="fixed mt-2 top-0 left-1/2 transform -translate-x-1/2 z-50 py-6 rounded-xl"
+    className="fixed mt-2 top-0 left-1/2 transform -translate-x-1/2 z-[5] py-6 rounded-xl"
 >
     <div className="container mx-auto">
         <div className="flex justify-center items-center gap-20 text-lg tracking-tighter font-semibold text-[--color-main-light]">
